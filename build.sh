5c35827550a79c792e9d6eece7bad6b99947a8f3
Geo Jacob <geojacobm6@gmail.com>
2019-01-04 21:36:47 +0530

    remove linux installer from build file

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm -rf build/
 rm -rf engine.spec
 npm run package-mac
 npm run create-installer-mac
-npm run package-linux
-npm run create-debian-installer
+#npm run package-linux
+#npm run create-debian-installer
