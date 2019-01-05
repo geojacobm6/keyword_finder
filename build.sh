4feaaac938e866449ddff4951112e777dfb1907f
Geo Jacob <geojacobm6@gmail.com>
2019-01-05 22:55:45 +0530

    windows installer files

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ rm -rf build/
 rm -rf engine.spec
 npm run package-mac
 npm run create-installer-mac
+npm run package-win
+node installers/windows/createinstaller.js
 #npm run package-linux
 #npm run create-debian-installer
